7d16ca2e5135b08ac992a64a550ba7866b663c69
Li Cheng <chengli@thoughtworks.com>
2016-11-17 12:37:54 +0800

    load session settings into redis conf

--- src/main/java/com/stardust/easyassess/assessment/conf/RedisSessionConfig.java
+++ src/main/java/com/stardust/easyassess/assessment/conf/RedisSessionConfig.java
@@ -1,10 +1,25 @@
 package com.stardust.easyassess.assessment.conf;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.RedisOperationsSessionRepository;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+import javax.annotation.PostConstruct;
+
 
 @Configuration
 @EnableRedisHttpSession
 public class RedisSessionConfig {
+    @Value("${server.session.timeout}")
+    private Integer maxInactiveIntervalInSecond;
 
+    @Autowired
+    private RedisOperationsSessionRepository sessionRepository;
+
+    @PostConstruct
+    private void afterPropertiesSet() {
+        sessionRepository.setDefaultMaxInactiveInterval(maxInactiveIntervalInSecond);
+    }
 }
