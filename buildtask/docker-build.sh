f6c9ff1f5aee62711d4a54f47b69edab0878380b
CHENG LI <rivneg@163.com>
2021-04-19 17:32:00 +1000

    WIP

--- buildtask/docker-build.sh
+++ buildtask/docker-build.sh
@@ -3,5 +3,7 @@
 export ES_ENV="prod"
 rm -rf target
 mvn clean package -Dmaven.test.skip=true
+pwd
+ls
 
 docker build --no-cache=true -f ./buildtask/Dockerfile -t registry.cn-beijing.aliyuncs.com/easyassess/assess-service:latest ./
